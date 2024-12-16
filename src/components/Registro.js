f985ae7bd4c4a578e25c15f0ff7d1ee5298f25ab
Leandro Cardozo <l6beeats@gmail.com>
2024-12-16 14:12:25 -0300

    cadastro pessoa

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -4,41 +4,85 @@ import { useNavigate } from "react-router-dom";
 
 export default function Registro() {
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
   const [nome, setNome] = useState("");
   const [cep, setCep] = useState("");
   const [logradouro, setLogradouro] = useState("");
   const [bairro, setBairro] = useState("");
   const [cidade, setCidade] = useState("");
   const [estado, setEstado] = useState("");
+  const [numero, setNumero] = useState("");
+  const [complemento, setComplemento] = useState("");
+  const [ibge, setIbge] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [cnpjCpf, setCnpjCpf] = useState("");
+  const [contato, setContato] = useState("");
+  const [razaoSocial, setRazaoSocial] = useState("");
   const [erro, setErro] = useState("");
   const navigate = useNavigate();
 
   const handleRegistro = async (e) => {
     e.preventDefault();
-
-    const body = { nome, email, senha, endereco: { cep, logradouro, bairro, cidade, estado } };
-
+    setErro("");
+  
+    // Configuração da requisição
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://equilibrioapperp.pontalsistemas.com.br/serverecommerce/NovaPessoa",
+      headers: {
+        "X-Embarcadero-App-Secret": "DE1BA56B-43C5-469D-9BD2-4EB146EB8473",
+      },
+      params: {
+        Grupo: "231",
+        Empresa: "371",
+        Token: "LOF2YBFRRPK5SO44TWQA",
+        CnpjCpf: cnpjCpf,
+        Nome: nome,
+        Contato: contato,
+        Endereco: logradouro,
+        Bairro: bairro,
+        Cidade: cidade,
+        Cep: cep,
+        UF: estado,
+        Email: email,
+        Numero: numero,
+        Complemento: complemento,
+        Ibge: ibge,
+        Telefone: telefone,
+        IE: "",
+        RazaoSocial: razaoSocial,
+      },
+    };
+  
     try {
-      const response = await axios.post("/api/registro", body);
-      if (response.data.success) {
-        navigate("/login"); // Redireciona para login após registro
+      const response = await axios.request(config);
+  
+      // Log da resposta no console
+      console.log("Resposta da API:", response.data);
+  
+      if (response.data && response.data.ID) {
+        // Exibir mensagem informando que o cadastro deu certo
+        alert(`Cadastro Realizado com Sucesso. ID: ${response.data.ID}`);
+        navigate("/login"); // Redireciona para login após registro 
       } else {
-        setErro("Erro ao registrar, tente novamente.");
+        alert("Erro ao registrar verifique seus dados!");
       }
+
     } catch (error) {
-      setErro("Erro ao registrar");
+      setErro("Erro ao registrar.");
       console.error(error);
     }
   };
 
   const buscarEndereco = async (e) => {
-    const cepValue = e.target.value;
+    const cepValue = e.target.value.replace(/\D/g, ""); // Remove caracteres não numéricos
     setCep(cepValue);
 
     if (cepValue.length === 8) {
       try {
-        const response = await axios.get(`https://viacep.com.br/ws/${cepValue}/json/`);
+        const response = await axios.get(
+          `https://viacep.com.br/ws/${cepValue}/json/`
+        );
         if (response.data.erro) {
           setErro("CEP não encontrado.");
         } else {
@@ -46,6 +90,8 @@ export default function Registro() {
           setBairro(response.data.bairro);
           setCidade(response.data.localidade);
           setEstado(response.data.uf);
+          setIbge(response.data.ibge);
+          setErro(""); // Limpa mensagem de erro
         }
       } catch (error) {
         setErro("Erro ao buscar endereço.");
@@ -59,40 +105,53 @@ export default function Registro() {
       <h2>Cadastro</h2>
       {erro && <p style={{ color: "red" }}>{erro}</p>}
       <form onSubmit={handleRegistro}>
-        {/* Campos de Login e Senha */}
         <input
           type="email"
-          placeholder="Login"
+          placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
-          type="password"
-          placeholder="Senha"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-        />
-        
-        {/* Campo Nome */}
         <input
           type="text"
           placeholder="Nome"
           value={nome}
           onChange={(e) => setNome(e.target.value)}
         />
-        
-        {/* Campos de Endereço */}
+        <input
+          type="text"
+          placeholder="Contato"
+          value={contato}
+          onChange={(e) => setContato(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="CNPJ/CPF"
+          value={cnpjCpf}
+          onChange={(e) => setCnpjCpf(e.target.value.replace(/\D/g, ""))} // Remove caracteres não numéricos
+        />
         <input
           type="text"
           placeholder="CEP"
           value={cep}
-          onChange={buscarEndereco} // Função de buscar endereço
+          onChange={buscarEndereco}
         />
         <input
           type="text"
           placeholder="Logradouro"
           value={logradouro}
           onChange={(e) => setLogradouro(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Número"
+          value={numero}
+          onChange={(e) => setNumero(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Complemento"
+          value={complemento}
+          onChange={(e) => setComplemento(e.target.value)}
         />
         <input
           type="text"
@@ -112,8 +171,26 @@ export default function Registro() {
           value={estado}
           onChange={(e) => setEstado(e.target.value)}
         />
-
-        {/* Botão de registro */}
+        <input
+          type="text"
+          placeholder="IBGE"
+          value={ibge}
+          onChange={(e) => setIbge(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Telefone"
+          value={telefone}
+          onChange={(e) => setTelefone(e.target.value)}
+        />
+        {cnpjCpf.length === 14 && (
+          <input
+            type="text"
+            placeholder="Razão Social (obrigatório para CNPJ)"
+            value={razaoSocial}
+            onChange={(e) => setRazaoSocial(e.target.value)}
+          />
+        )}
         <button type="submit">Registrar</button>
       </form>
       <p>
